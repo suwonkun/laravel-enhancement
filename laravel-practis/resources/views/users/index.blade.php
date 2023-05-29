72a2a652112af2d877a5d1dacb1c33a6c14e29ec
nakazawashion <nakazawa.shion@celsys.com>
2023-05-29 18:23:46 +0900

    ユーザー一覧画面の表示並び替え

--- laravel-practis/resources/views/users/index.blade.php
+++ laravel-practis/resources/views/users/index.blade.php
@@ -1,21 +1,21 @@
 <table>
     <thead>
     <tr>
+        <th>名前</th>
         <th>会社</th>
         <th>部署</th>
-        <th>名前</th>
     </tr>
     </thead>
     <tbody>
     @foreach($users as $user)
         <tr>
+            <td>{{ $user->name }}</td>
             <td>{{ $user->company->name }}</td>
             <td>
                 @foreach($user->sections as $section)
                     {{ $section->name }}<br>
                 @endforeach
             </td>
-            <td>{{ $user->name }}</td>
         </tr>
     @endforeach
     </tbody>
