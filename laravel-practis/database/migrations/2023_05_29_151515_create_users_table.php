0fdf052765cd56b5756b498bd1ce39870cdcd01d
nakazawashion <nakazawa.shion@celsys.com>
2023-05-29 18:09:14 +0900

    userのroleをvarcharに変更

--- laravel-practis/database/migrations/2023_05_29_151515_create_users_table.php
+++ laravel-practis/database/migrations/2023_05_29_151515_create_users_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['admin', 'user'])->default('user')->comment('権限');
+            $table->string('role')->default('user')->comment('権限');
         });
     }
 
@@ -21,6 +20,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+        });
     }
 };
