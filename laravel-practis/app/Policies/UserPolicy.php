d68ebf85ea963bf222b279ad72163a0f84d451db
nakazawashion <nakazawa.shion@celsys.com>
2023-05-30 18:46:51 +0900

    認可の実装

--- laravel-practis/app/Policies/UserPolicy.php
+++ laravel-practis/app/Policies/UserPolicy.php
@@ -8,12 +8,18 @@ use Illuminate\Auth\Access\Response;
 
 class UserPolicy
 {
+    public function before(User $user, $ability)
+    {
+        if ($user->is_admin) {
+            return true;
+        }
+    }
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(User $user, Company $company): bool
     {
-        return true;
+        return $user->company_id === $company->id;
     }
 
 }
